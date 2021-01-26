9e2803447b4fe57882a0c5431c2d3ae2e3b46a7f
Tong - Pongpun Tipyangul <tong@bluestonepim.com>
2021-01-26 18:20:25 +0700

    Styling new card on category page

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -9,10 +9,13 @@ class NewsCard extends Component {
             let newsImageCardClassName = classes.newsCard + ' index' + this.props.index;
             return (
                 <Link className={classes.newCardLink} to={`/article/${this.props.newsId}`}>
-                    <div className={newsImageCardClassName}>
-                        <img src={this.props.img} /><br />
-                        <div className={classes.newsTitle}>{this.props.title}</div>
-                        <div dangerouslySetInnerHTML={{__html: this.props.body}} />
+                    <div className={newsImageCardClassName} style={{ backgroundImage: `url(${this.props.img})` }}>
+                        <div className={classes.inner}>
+                            <div className={classes.newsTitle}>{this.props.title}</div>
+                            {/* <img src={this.props.img} /><br /> */}
+                            {/* <div className={classes.newsTitle}>{this.props.title}</div>
+                            <div dangerouslySetInnerHTML={{__html: this.props.body}} /> */}
+                        </div>
                     </div>
                 </Link>
             );
